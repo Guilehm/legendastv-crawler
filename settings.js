6c9f9ea6b8bd43f64223fba36d8ab2fbc299ec6d
guilherme <guile.hm@hotmail.com>
2020-09-19 12:00:49 -0300

    create settings file

---
+++ settings.js
@@ -0,0 +1,10 @@
+const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+const VIEWPORT = { width: 1366, height: 768 }
+const OPTIONS = { headless: false, args: ['--start-maximized'] }
+
+
+module.exports = {
+    USER_AGENT,
+    VIEWPORT,
+    OPTIONS,
+}

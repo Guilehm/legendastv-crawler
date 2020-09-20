deeeb963bc90549fd36e2c49ad3a959ca26a028a
guilherme <guile.hm@hotmail.com>
2020-09-20 14:01:56 -0300

    create db, app and logger files

---
+++ app.js
@@ -0,0 +1,44 @@
+const mongo = require('./database/mongodb')
+const { program } = require('commander')
+const SubtitleService = require('./services/subtitle-service')
+const Subtitle = require('./database/models/subtitle-model')
+const Title = require('./database/models/title-model')
+const logger = require('./utils/logger')
+
+const service = new SubtitleService()
+
+program
+    .option('-c, --crawl')
+    .option('-u, --update')
+    .option('--count')
+    .option('-t, --text <type>', 'Nome do filme', 'Os Simpsons')
+
+program.parse(process.argv)
+
+
+const run = async () => {
+    try {
+        if (program.crawl) {
+            await service.getAndSaveTitles(program.text)
+            const [totalCountTitles,] = await service.countItems(Title)
+            logger.info(`BUSCANDO LEGENDAS PARA ${totalCountTitles} TÍTULOS`)
+            await service.getAndSaveSubtitleLinks()
+            const [totalCountSUbtitles,] = await service.countItems(Subtitle)
+            logger.info(`FORAM ENCONTRADAS ${totalCountSUbtitles} LEGENDAS.`)
+        } else if (program.update) {
+            await service.getAndUpdateSubtitles()
+        } else if (program.count) {
+            const [totalCount, crawledCount] = await service.countItems(Subtitle)
+            logger.info(`FORAM ENCONTRADAS ${totalCount} LEGENDAS.`)
+            logger.info(`LEGENDAS ATUALIZADAS: ${crawledCount}/${totalCount}.`)
+        }
+    } catch (err) {
+        logger.err(err.message)
+        logger.info('OCORREU UM ERRO, POR FAVOR TENTE NOVAMENTE')
+        process.exit(1)
+    } finally {
+        service.crawler.browser ? service.crawler.browser.close() : ''
+        mongo.connection.close()
+    }
+}
+run()
--- database/mongodb.js
+++ database/mongodb.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
-
+const logger = require('../utils/logger')
 
 const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-    .then(() => console.log('Connected to Mongo'))
-    .catch(e => console.log('Something went wrong', e))
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+    .then(() => logger.info('Conectado ao Mongo'))
+    .catch(e => logger.error('Ocorreu um erro na conexão com o mongo', e))
 
 
 module.exports = mongoose
